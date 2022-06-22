2375826c22012e313e23da96e40c8b696fd38430
Podushkin Vitaliy <plasticfiresam@gmail.com>
2022-06-22 15:56:05 +0700

    added fonts and golden_toolkit

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:get_it/get_it.dart';
 import 'package:waifu/app_model.dart';
 import 'package:waifu/service/navigation_helper.dart';
@@ -8,6 +10,12 @@ import 'package:waifu/waifu_app.dart';
 GetIt getIt = GetIt.instance;
 
 void main() {
+  /// for google_fonts purposes
+  LicenseRegistry.addLicense(() async* {
+    final license = await rootBundle.loadString('google_fonts/OFL.txt');
+    yield LicenseEntryWithLineBreaks(['google_fonts'], license);
+  });
+
   getIt
     ..registerSingleton<AppModel>(AppModel())
     ..registerSingleton<WaifuService>(WaifuService())
